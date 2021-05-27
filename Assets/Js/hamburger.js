e21011224ff58a2e45ef364cf25e205ef933cdb6
ADEBOYE TOSIN <tosinat2009@gmail.com>
2021-05-27 06:42:45 +0100

    first commit

---
+++ Assets/Js/hamburger.js
@@ -0,0 +1,8 @@
+// GET ACCESS TO THE NAV-ITEM CONTAINER FROM THE HTML
+const navItemContainer = document.getElementById("nav-items-wrapper")
+
+// NAV BAR OPEN AND CLOSE FUNCTION 
+function toggleNavbar(){
+    navItemContainer.classList.toggle("open-nav-items")
+}
+
